2405c2b7019d2425109744aa0643740e05d5d78c
Jeff Sharpe <jsharp18@uncc.edu>
2016-11-09 03:10:59 +0000

    Done with part 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,9 +1,19 @@
 # Add a declarative step here for populating the DB with movies.
 
+counter = 0
+
 Given /the following movies exist/ do |movies_table|
+  
+  counter = 0
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+      
+    Movie.create(movie)
+    counter += 1
+    
+    # end
+    
   end
   fail "Unimplemented"
 end
@@ -21,7 +31,7 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: "(.*)"/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
